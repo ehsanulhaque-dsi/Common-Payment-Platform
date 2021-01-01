3a152fd0762301d4180c0289e73a23a90d82d42f
Ehsanul.dsi <ehsanuldsi@L-BKASH-2884-UBU.lan>
2021-01-01 22:03:11 +0600

    initial commit

---
+++ cypress/integration/cpp/customer.js
@@ -0,0 +1,5 @@
+describe('The Home Page', () => {
+    it('successfully loads', () => {
+      cy.visit('https://docs.cypress.io/guides/getting-started/testing-your-app.html#Step-3-Configure-Cypress')
+    })
+  })
