4d4e0d0ca731b8c655df3b3da96c7f4566bfb7aa
Ehsanul.dsi <ehsanuldsi@L-BKASH-2884-UBU.lan>
2021-01-03 11:49:50 +0600

    Customer full flow automated

--- cypress/integration/cpp/customer.js
+++ cypress/integration/cpp/customer.js
@@ -1,5 +1,70 @@
+import 'cypress-iframe'
+
 describe('The Home Page', () => {
-    it('successfully loads', () => {
-      cy.visit('https://docs.cypress.io/guides/getting-started/testing-your-app.html#Step-3-Configure-Cypress')
+
+  it('successfully loads', () => {
+    cy.visit('https://customer-cpp.labs.bka.sh/ehsan/cokeshop/', {
+      failOnStatusCode: false
     })
   })
+
+  for (var i = 0; i < 5; i++) {
+    it('Click the pay now button', () => {
+      cy.get('.v-btn').click()
+    })
+
+    it('Fill valid payment information ', () => {
+      var amount = Math.floor((Math.random() * 500) + 1);
+
+      cy.get('[name=amount]').type(amount)
+      cy.get('[name=customerPhoneNumber]').type("01987603822")
+
+      cy.get('.v-btn').click()
+    })
+
+    it('Process payment using bkash', () => {
+
+      cy.wait(7000)
+      cy.frameLoaded()
+
+      cy.get('iframe')
+        .then(($iframe) => {
+          const $body = $iframe.contents().find('body')
+
+          cy.wrap($body)
+            .find('#wallet')
+            .type('01770618575')
+
+          cy.wrap($body)
+            .find('#submit_button')
+            .click()
+
+          cy.wrap($body)
+            .find('#otp')
+            .type('123456')
+
+          cy.wrap($body)
+            .find('#submit_button')
+            .click()
+
+          cy.wrap($body)
+            .find('#pin')
+            .should('be.visible')
+            .type('12121')
+
+          cy.wrap($body)
+            .find('#submit_button')
+            .click()
+        })
+
+      cy.get('.v-btn--contained', {
+        timeout: 15000
+      }).should('be.visible').click()
+
+
+
+    })
+  }
+
+
+})
